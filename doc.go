14b6762f6847a707c766caceaff1fede4baeacea
Kerby Shedden <kshedden@umich.edu>
2019-04-06 10:33:21 -0400

    update

--- doc.go
+++
@@ -1,6 +0,0 @@
-/* statmodel is a set of type definitions and generic functions to
-support packages that fit statistical models.  It does not implement
-any specific statistical models, see http://github.com/kshedden/goglm"
-for a specific implementation for Generalized Linear Models.  */
-
-package statmodel
